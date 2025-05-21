19ee04038f564a10271582f55964fdeb856b54b8
Kent Russel Balajadia <kentggrussel@gmail.com>
2025-05-22 06:01:45 +0800

    logo title

--- obj/Debug/net9.0/AStarAlgorithmDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/AStarAlgorithmDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AStarAlgorithmDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2792e49c907d36599aeb98d7e295d85e343f7802")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a7351081b5825f7600477e1cd24b419ff97cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("AStarAlgorithmDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AStarAlgorithmDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
